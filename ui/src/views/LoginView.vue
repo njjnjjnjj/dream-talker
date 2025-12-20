3e2126cfe370a7955e3aa8d7104fee0418b26ff1
nijiajun <njj1108@outlook.com>
2025-12-20 14:26:43 +0800

    refactor: 调整分割线

--- ui/src/views/LoginView.vue
+++ ui/src/views/LoginView.vue
@@ -243,12 +243,12 @@ const skipBiometricSetup = () => {
           <div class="absolute inset-0 flex items-center" aria-hidden="true">
             <div class="w-full border-t border-slate-700" />
           </div>
-          <div class="relative flex justify-center">
+          <!-- <div class="relative flex justify-center">
             <span class="bg-slate-900 px-2 text-sm text-slate-500">Or</span>
-          </div>
+          </div> -->
         </div>
         
-        <div v-if="isBiometricAvailable" class="pt-2">
+        <div v-if="isBiometricAvailable">
            <button
             type="button"
             @click="handleBiometricLogin"
